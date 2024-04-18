9ede5c515832249a1bb1817c053dc757ba18bd72
Nathanael Page <defensoft@gmail.com>
2024-04-18 00:40:13 -0400

    under construction page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="title-box"><img className="logo" src="logo.png" /><h1 className="title">Nate's Games</h1></div>
+      <h2 className="subtitle">Under Construction</h2>
     </div>
   );
 }
